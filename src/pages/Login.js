2d8e33d575b9f2f12e7f2118075d5c4a0fdabdfe
JustonSmith <75957152+JustonSmith@users.noreply.github.com>
2021-11-25 00:14:08 -0600

    Login and Registration

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,48 @@
-import React from 'react'
-import DefaultLayout from '../components/DefaultLayout'
-import {Row, Col, Form, Input} from 'antd'
+import React from 'react';
+import { Row, Col, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { userLogin } from '../redux/actions/userActions';
 
 function Login() {
+    const dispatch = useDispatch()
+    function onFinish(values) {
+        dispatch(userLogin(values))
+        console.log(values)
+    }
+
     return (
         <div className= 'login'>
                 
-            <Row gutter={16}>
+            <Row gutter={16} className= 'd-flex align-items-center'>
 
-                <Col lg={16}></Col>
-                <Col lg={8} className="text-left">
-                    <Form layout='vertical'>
+                <Col lg={16}>
+                    <img className="login-img" src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt="login"/> 
+                    <h1 className="login-logo">uDrive</h1>
+                </Col>
+                <Col lg={8} className="text-left p-5">
+                    <Form layout='vertical' className='login-form p-5' onFinish= {onFinish}>
                         <h1>Login</h1>
                         <hr />
 
-                        <Form.Item name= 'username' label= 'username' rules={[{required:true}]}>
+                        <Form.Item 
+                        name= 'username' 
+                        label= 'username' 
+                        rules={[{required:true}]}
+                        >
                             <Input />
                         </Form.Item>
-                        <Form.Item name= 'password' label= 'password' rules={[{required:true}]}>
+                        <Form.Item 
+                        name= 'password' 
+                        label= 'password' 
+                        rules={[{required:true}]}
+                        >
                             <Input />
                         </Form.Item>
 
-                        <button className= "btn1">Login</button>
+                        <button className= "btn1 mt-2">Login</button>
+                        <hr />
+                        <Link to= '/register'> Don't have an account?</Link>
 
                     </Form>
                 </Col>
